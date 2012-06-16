ba72aff9d14487b40a182923e0e6f5d01858141c
David Mamber <dmamber@globeandmail.com>
2012-06-16 13:19:35 -0400

    Initial Pages

---
+++ desktop/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Blank</title>
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" href="css/style.css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>		
+	</head>
+	<body>
+		
+	</body>
+</html>
