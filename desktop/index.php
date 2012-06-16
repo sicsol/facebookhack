edb32b4db18d7856b859a8d6e73af12cd0a9d7da
David Mamber <dmamber@globeandmail.com>
2012-06-16 13:36:07 -0400

    Created SCSS and assets Created file structure with includes

--- desktop/index.php
+++ desktop/index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Blank</title>
-		<meta charset="UTF-8"/>
-		<link rel="stylesheet" href="css/style.css" />
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>		
+		<? include('includes/title.php'); ?>
 	</head>
 	<body>
 		
+		<? include('includes/header.php'); ?>
+		
+		<section>
+			body
+		</section>
+		
+		<? include('includes/footer.php'); ?>
+
 	</body>
 </html>
---
+++ desktop/pledge.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<? include('includes/title.php'); ?>
+	</head>
+	<body>
+		
+		<? include('includes/header.php'); ?>
+		
+		<section>
+			body
+		</section>
+		
+		<? include('includes/footer.php'); ?>
+
+	</body>
+</html>
